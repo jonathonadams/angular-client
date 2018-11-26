24799b04ed2220af515e81387326e4200f789f15
Jonathon Adams <jono.adams22@gmail.com>
2018-11-26 20:46:01 +1030

    Added a Todo TNS routing module.

---
+++ src/app/features/todos/todos-routing.module.tns.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+import { TodosComponent } from './components/todos.component';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+
+const routes: Routes = [{ path: '', pathMatch: 'full', component: TodosComponent }];
+
+@NgModule({
+  imports: [NativeScriptRouterModule.forChild(routes)],
+  exports: [NativeScriptRouterModule]
+})
+export class TodoRoutingModule {}
