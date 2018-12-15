21d0c22e771dceebb43340aa799f2f711ee5dee2
Jonathon Adams <jono.adams22@gmail.com>
2018-12-15 12:27:51 +1030

    Removed and updated dependencies and small styling changes to the todo detail component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { reducers, metaReducers, AppState } from '@store/reducers';
 import { ComponentsModule } from '@components/components.module';
 import { AuthModule } from './auth';
 import { CoreModule } from '@app-core/core.module';
 import { NavigationModule } from './navigation/navigation.module';
-import { environment } from '~/environments/environment';
 import { appEffects } from './store';
 
 @NgModule({
@@ -22,12 +20,6 @@ import { appEffects } from './store';
     BrowserModule,
     BrowserAnimationsModule,
     StoreModule.forRoot<AppState>(reducers, { metaReducers }),
-    !environment.production
-      ? StoreDevtoolsModule.instrument({
-          maxAge: 25, // Retains last 25 states
-          logOnly: environment.production // Restrict extension to log-only mode
-        })
-      : [],
     EffectsModule.forRoot(appEffects),
     CoreModule.forRoot(),
     AuthModule.forRoot(),
