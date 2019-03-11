589b8532967c02fe25f651fc3ecd02f6e6c63e5d
Jonathon Adams <jono.adams22@gmail.com>
2019-03-12 08:41:52 +1030

    Updated the e2e tests

--- e2e/config.e2e.ts
+++ e2e/config.e2e.ts
@@ -6,3 +6,13 @@
  * URL for site
  */
 export const SITE_URL = 'http://localhost:4200';
+
+export const AUTHENTICATED_USER = {
+  username: 'admin',
+  password: 'secret'
+};
+
+export const UN_AUTHENTICATED_USER = {
+  username: 'some user',
+  password: 'incorrect password'
+};
--- e2e/src/login/login.e2e-spec.ts
+++ e2e/src/login/login.e2e-spec.ts
@@ -1,4 +1,3 @@
-import { ClientFunction } from 'testcafe'; // first import testcafe selectors
 import { waitForAngular } from 'testcafe-angular-selectors';
 import { LoginPageObject } from './login.po';
 import { SITE_URL } from '../../config.e2e';
@@ -10,41 +9,26 @@ fixture(`Login Fixture`)
     await waitForAngular();
   });
 
-const getPageUrl = ClientFunction(() => window.location.href);
-const localStorageSet = ClientFunction((key, val) => localStorage.setItem(key, val));
-const localStorageGet = ClientFunction(key => localStorage.getItem(key));
-
 test('should log a user in and redirect to the home page', async t => {
   const page: LoginPageObject = t.ctx.loginPage;
 
-  await t.expect(await localStorageGet('access_token')).notOk();
+  await page.verifyUserIsNotLoggedIn();
   await page.login();
-
-  await t.expect(localStorageGet('access_token')).ok();
-  await t.expect(localStorageGet('access_token')).typeOf('string');
-  await t.expect(getPageUrl()).contains('home');
-  await t.expect(getPageUrl()).notContains('login');
+  await page.verifyUserIsAuthenticated();
+  await page.verifyHomeRedirectFromLoginPage();
 });
 
 test('should not authenticate a user with incorrect credentials', async t => {
   const page: LoginPageObject = t.ctx.loginPage;
 
-  await t
-    .typeText(page.usernameInput, 'some user')
-    .typeText(page.passwordInput, 'and one')
-    .click(page.singInButton)
-    .expect(getPageUrl())
-    .contains('login')
-    .expect(getPageUrl())
-    .notContains('home');
+  await page.loginWithIncorrectCredentials();
+  await page.verifyNoHomeRedirect();
 });
 
-test('should redirect to home if allready signed in', async t => {
+test('should redirect to home if already signed in', async t => {
   const page: LoginPageObject = t.ctx.loginPage;
+
   await page.login();
-
   await page.navigateTo();
-
-  await t.expect(getPageUrl()).notContains('login');
-  await t.expect(getPageUrl()).contains('home');
+  await page.verifyHomeRedirectFromLoginPage();
 });
