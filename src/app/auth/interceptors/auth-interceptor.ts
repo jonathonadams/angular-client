e46e7bcb45abdd4a1bc7d727c7a83d2cbe9394fe
Jonathon Adams <jono.adams22@gmail.com>
2019-04-07 19:13:38 +0930

    Added error handling to the auth interceptor.

--- src/app/auth/interceptors/auth-interceptor.ts
+++ src/app/auth/interceptors/auth-interceptor.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from '@auth/services/auth.service';
+import { catchError } from 'rxjs/operators';
+import { AuthFacade } from '../services/auth.facade.service';
+import { GlobalFacade } from '~/app/store/effects/global.facade';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService,
+    private facade: AuthFacade,
+    private globalFacade: GlobalFacade
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // Get the auth token from the service.
@@ -12,11 +20,21 @@ export class AuthInterceptor implements HttpInterceptor {
 
     // Clone the request and replace the original headers with
     // cloned headers, updated with the authorization.
-    const authReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${authToken}`)
-    });
-
+    const authReq = req.clone({ setHeaders: { Authorization: `Bearer ${authToken}` } });
     // send cloned request with header to the next handler.
-    return next.handle(authReq);
+    return next.handle(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.facade.logout();
+          this.globalFacade.displayNotification('Your session has expired, please login.');
+        } else if (error.status === 404) {
+          this.facade.logout();
+          this.globalFacade.displayNotification(
+            'An error has occurred, please login and try again.'
+          );
+        }
+        return throwError(error);
+      })
+    );
   }
 }
